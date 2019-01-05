1620fcab8e1eeefb30deff2b1b8cf1492aeedb66
Pawel Targosz <ptargosz@squiz.pl>
2019-01-05 20:42:32 +1100

    GraphQL Basic Types

--- src/index.js
+++ src/index.js
@@ -2,16 +2,61 @@ const { ApolloServer, gql}  = require('apollo-server');
 
 //1 create graphql schema 
 const typeDefs = gql`
-
     type Query {
         hello: String!
+        user: User
+    }
+
+    type User {
+        id: ID!
+        username: String!
+    }
+
+    type Error {
+        field: String!
+        message: String!
+    }
+
+    type RegisterResponse {
+        errors: [Error!]!
+        user: User
+    }
+
+    input UserInfo {
+        username: String!
+        password: String!
+        age: Int
+    }
+
+    type Mutation {
+        register(userInfo: UserInfo): RegisterResponse!
+        login(userInfo: UserInfo): Boolean!
     }
 `; 
 
 //2 resolvers
 const resolvers = {
     Query: {
-        hello: () => 'hello word'
+        hello: () => 'Hello Word!',
+        user: () => ({
+            id: 1,
+            username: 'Pawel'
+        })
+    },
+    Mutation: {
+        login: () => true,
+        register: () => ({
+            errors: [
+                {
+                    field: 'username',
+                    message: 'bad'
+                }
+            ],
+            user: {
+                id: 1,
+                username: 'Pawel'
+            }
+        })
     }
 }
 
